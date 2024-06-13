b6a1e5261b561d1d56f25b6b302c3d4eb4d9f204
Щепелин Данил <schepelind@echo-company.ru>
2024-06-13 23:08:04 +0500

    fix: fix todo item model

--- model/todo-item.go
+++ model/todo-item.go
@@ -1,15 +1,16 @@
 package model
 
 import (
+	"gorm.io/gorm"
 	_ "gorm.io/gorm"
 	"time"
 )
 
 type TodoItem struct {
-	ID          uint `gorm:"primarykey"`
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	DeletedAt   time.Time
+	ID          uint   `gorm:"primarykey"`
 	Title       string `gorm:"not null" json:"title"`
 	Description string `gorm:"not null" json:"description"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	DeletedAt   gorm.DeletedAt `gorm:"index"`
 }
